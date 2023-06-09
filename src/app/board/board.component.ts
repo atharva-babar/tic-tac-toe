d7e385b87fa27ce6e6215e912883e2add41e5209
Atharva Babar <atharva.babar@coditas.com>
2023-06-09 14:59:07 +0530

    completed app

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css'],
+})
+export class BoardComponent implements OnInit {
+  squares: any[];
+  xIsNext: boolean;
+  winner: string;
+  isTie: boolean;
+
+  constructor() {}
+  ngOnInit(): void {
+    this.newGame();
+  }
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = '';
+    this.xIsNext = true;
+    this.isTie = false;
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  makeMove(idx: number) {
+    if (!this.squares[idx]) {
+      this.squares.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    }
+    this.winner = this.calculateWinner();
+    if (!this.squares.includes(null) && this.winner === null) {
+      this.isTie= true
+    }
+  }
+
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [0, 4, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 4, 5],
+      [6, 7, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+}
